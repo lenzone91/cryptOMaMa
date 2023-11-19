a9bee90f35e22dcdd98fcfeefd669f945927932b
enzo <cognenzo@gmail.com>
2023-11-19 18:25:00 +0100

    main correction

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-#OMaMa Version 0.0.1
+"""
+OMaMa Version 0.0.1
+"""
 
 import traceback
-from src.utils.args_reading import *
+from src.utils.args_reading import process_arguments
 from src.launcher import Launcher
 
 
@@ -16,5 +18,5 @@ if __name__ == "__main__":
     except Exception as e:
         print("Terminated unsuccessfully")
         traceback.print_exc()
-        raise Exception("Terminated unsuccessfully")
+        raise Exception("Terminated unsuccessfully") from e
         
