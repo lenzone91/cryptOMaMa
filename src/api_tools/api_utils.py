d85cd5b23b1ef5fc55def0da3f41a83a1f3b31fc
enzo <cognenzo@gmail.com>
2023-11-19 18:36:20 +0100

    Some pylint corrections

--- src/api_tools/api_utils.py
+++ src/api_tools/api_utils.py
@@ -1,10 +1,17 @@
+"""
+Useful methods for managing all APIs like a factory
+"""
+
 from src.api_tools.brokers.binance import Binance
 
 def create_broker(api, api_key, private_key):
+    """
+    Factory method to create the correct instance of an API class
+    """
+
     if api == "binance":
         return Binance(api_key, private_key)
     #elif api == "cryptocom":
     #    return CryptoCom(api_key, private_key)
     # Ajoutez d'autres cas selon les besoins pour de nouveaux brokers
-    else:
-        raise ValueError(f"Unsupported broker type: {api}")
+    raise ValueError(f"Unsupported broker type: {api}")
--- src/api_tools/broker.py
+++ src/api_tools/broker.py
@@ -1,27 +1,45 @@
+"""
+Abstract base class for Brokers
+"""
+
 from abc import ABC, abstractmethod
 
 class Broker(ABC):
-    
+    """
+    Abstract base class for Brokers
+    """
+
     @abstractmethod
     def post_request(self):
+        """
+        Abstract method for handling POST requests.
+        """
         pass
 
     @abstractmethod
     def get_request(self):
+        """
+        Abstract method for handling GET requests.
+        """
         pass
 
     @abstractmethod
     def get_avgPrice(self, symbol):
+        """
+        Abstract method for getting average price.
+        """
         pass
 
     @abstractmethod
     def ping(self):
+        """
+        Abstract method for handling ping requests.
+        """
         pass
 
     @abstractmethod
     def get_depth(self, symbol):
+        """
+        Abstract method for getting depth information.
+        """
         pass
-
-
-
-
